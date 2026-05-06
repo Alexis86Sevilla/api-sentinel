4ffa719b1cc2484eaa48081587e329b5dc53916c
alexis <alexis@NUNLTS1404.NUNSYS.INT>
2026-05-06 16:08:42 +0200

    chore: backend installed

---
+++ backend/src/test/java/com/sentinel/inspector/ApiInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.inspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
